f6a26616953cf130e766f38e8a53758aeb697d56
Roneet Kumar <roneetparida@gmail.com>
2019-02-17 15:13:27 -0500

    sliders are linked with range values

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,23 @@
 new Vue({
     el: '#wrapper',
     data: {
-        name: 'Roneet',
-        job: 'ninja',
-        age: 25,
-        x: 0,
-        y: 0
+        sliderValue: [{
+            val: 20,
+            name: 'Length'
+        }, {
+            val: 50,
+            name: 'Symbol'
+        }, {
+            val: 70,
+            name: 'Digit'
+        }, {
+            val: 40,
+            name: 'Words'
+        }]
     },
-    methods: {
-        greet: function(time) {
-
-            return 'Good ' + time + ' ' + this.name;
-        },
-
-        add: function(inc) {
-            this.age += inc;
-        },
-
-        sub: function(dec) {
-            this.age -= dec;
-        },
-
-        update: function(event) {
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-
-        click: function() {
-            alert('You Clicked Me')
-        },
-
-        logname: function() {
-
-        },
-
-        logage: function() {
-
+    computed: {
+        total() {
+            return (this.sliderValue[3].val + this.sliderValue[1].val + this.sliderValue[2].val + this.sliderValue[3].val) / 4;
         }
     }
-});
+})
